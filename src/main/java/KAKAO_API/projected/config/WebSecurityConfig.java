dbfbf20a1732662f537e1698eb6f8c3431b33858
Theo <ojw1912@gmail.com>
2025-03-25 17:29:25 +0900

    250325_#2-2-3_E[Refac2]_JWT(Access/Refresh Token) .apply() 간접 → addFilterBefore() 직접 지정 | 체인多유지보수↑/람다기반 DSL로 인한 충돌

--- src/main/java/KAKAO_API/projected/config/WebSecurityConfig.java
+++ src/main/java/KAKAO_API/projected/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package KAKAO_API.projected.config;
 
 import KAKAO_API.projected.auth.jwt.JwtAuthenticationFilter;
-import KAKAO_API.projected.config.JwtSecurityConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +8,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
 
-    private final JwtSecurityConfig jwtSecurityConfig;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -26,16 +26,15 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf().disable()  // CSRF 보호 비활성화 (POST 테스트용)
-                .apply(jwtSecurityConfig) // ✅ Jwt 설정 모듈 조립
-                .and() // 설정 블록 종료 후 다음 설정으로 이동
+                .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화 (POST 테스트용) 람다기반 DSL
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/email/**").permitAll()  // ✅이메일 인증 허용!
                         .requestMatchers("/api/user/send-code").permitAll()  // ✅회원가입 인증코드 전송 허용
                         .requestMatchers("/api/user/verify-code").permitAll()  // ✅인증코드 검증 회원가입 진행 허용
                         .requestMatchers("/api/login").permitAll()  // ✅로그인 진행 허용
                         .anyRequest().authenticated()  // 나머지는 인증 필요
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // ✅ 명시적 필터 등록
 
         return http.build();
     }
