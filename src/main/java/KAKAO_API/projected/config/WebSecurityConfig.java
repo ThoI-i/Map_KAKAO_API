61397281ac85a5a3b315b91ed7572728582be083
Theo <ojw1912@gmail.com>
2025-03-25 16:06:30 +0900

    250325_#2-2-3_E[ERR]_JWT(Access/Refresh Token) 토큰 필터 순서 조정자 추가[ERROR 생성자주입 불가(Parameter 0 of constructor in JwtSecurityConfig required a bean of type 'JwtAuthenticationFilter' that could not be found. )]

--- src/main/java/KAKAO_API/projected/config/WebSecurityConfig.java
+++ src/main/java/KAKAO_API/projected/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package KAKAO_API.projected.config;
 
+import KAKAO_API.projected.auth.jwt.JwtAuthenticationFilter;
+import KAKAO_API.projected.config.JwtSecurityConfig;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,8 +10,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
+
+    private final JwtSecurityConfig jwtSecurityConfig;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -18,8 +24,11 @@ public class WebSecurityConfig {
     // ✨ 이 부분 추가!
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf().disable()  // CSRF 보호 비활성화 (POST 테스트용)
+                .apply(jwtSecurityConfig) // ✅ Jwt 설정 모듈 조립
+                .and() // 설정 블록 종료 후 다음 설정으로 이동
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/email/**").permitAll()  // ✅이메일 인증 허용!
                         .requestMatchers("/api/user/send-code").permitAll()  // ✅회원가입 인증코드 전송 허용
