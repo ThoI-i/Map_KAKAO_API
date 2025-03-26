99601aa3be01c5df76bbc431ae5bd4e4a4c1ec32
Theo <ojw1912@gmail.com>
2025-03-26 14:43:26 +0900

    250326_#3_0_CORS[Cross-Origin Resource Sharing(동일 출처 정책 설정(CorsConfig.java)]

---
+++ src/main/java/KAKAO_API/projected/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package KAKAO_API.projected.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173") // 프론트 포트
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true); // 쿠키 포함 허용!
+    }
+}
